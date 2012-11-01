1e3d2baf1066c5a8775b65bc20e776ba4dd2efbb
Sam LR <sam.lindenrathen@gmail.com>
2012-11-01 14:29:43 +0000

    added output folder and increased to 1M initial particles

--- Simple_muon_sim/dirty2.C
+++ Simple_muon_sim/dirty2.C
@@ -1,7 +1,7 @@
 void dirty2(){
-    // TFile* in_file = new TFile("air_out_100k.root", "READ");
-    // TFile* in_file = new TFile("copper_out_100k.root", "READ");
-    TFile* in_file = new TFile("galactic_out_100k.root", "READ");
+    TFile* in_file = new TFile("root/sep_6mm_st-x_3mm_st-mat_Galactic_n-particles_1000000_out.root", "READ");
+    // TFile* in_file = new TFile("root/sep_6mm_st-x_1mm_st-mat_Cu_n-particles_1000000_out.root", "READ");
+    // TFile* in_file = new TFile("root/sep_6mm_st-x_3mm_st-mat_AIR_n-particles_1000000_out.root", "READ");
     int nhit;
     int pdgid[500];
     int counter[500];
@@ -20,15 +20,15 @@ void dirty2(){
     const int n_entries = tree->GetEntries();
     
     TH1D* hist_raw = new TH1D("dt","dt",20000,0,20000);
-    TH1D* hist_rebin = new TH1D("dt_rebin","dt_rebin",200,50,20050);
+    TH1D* hist_rebin = new TH1D("dt_rebin","dt_rebin",100,50,20050);
     for(int entry = 0; entry < n_entries; ++entry) {
         tree->GetEntry(entry);
         if(nhit<2) continue;
         int mu_id = -1;
         double t0=-1.0;
         for(int hit = 0; hit < nhit; ++hit) {
-            bool mu_at_1 = (counter[hit]==2) && (pdgid[hit]==-13);
-            bool e_at_2  = (pdgid[hit]==-11) && parentid[hit]==mu_id; // t0 to confirm we've seen a mu
+            bool mu_at_1 = (counter[hit]==2) && (pdgid[hit]==13);
+            bool e_at_2  = (pdgid[hit]==11) && parentid[hit]==mu_id; // t0 to confirm we've seen a mu
             if (mu_at_1) {
                 t0 = tof[hit];
                 mu_id = trkid[hit];
--- Simple_muon_sim/run.sh
+++ Simple_muon_sim/run.sh
@@ -11,22 +11,21 @@ export G4NEUTRONXSDATA=/usr/local/share/Geant4-9.5.0/data/G4NEUTRONXS1.1
 export G4PIIDATA=/usr/local/share/Geant4-9.5.0/data/G4PII1.3
 export G4RADIOACTIVEDATA=/usr/local/share/Geant4-9.5.0/data/RadioactiveDecay3.4
 export G4REALSURFACEDATA=/usr/local/share/Geant4-9.5.0/data/RealSurface1.0
-# export G4UI_USE=1
-# export G4VIS_USE=1
 
 exe="../build/Release/Simple_muon_sim"
 log="> /dev/null"
-n_particles=500000
+n_particles=1000000
 separation=6
 # st_x=1 
 st_mat=( "3 Galactic" "3 AIR" "1 Cu" ) # will prepend the required 'G4_'
 file_name="out.root" # will prepend the other info
+output_dir="root/"
 
 for mat in "${st_mat[@]}";
 do
     # echo ${mat/\ /_} substitue the ' ' between st-x & st-mat for a '_'
     # also used to sub ' G4_' for the actual command
-    file="sep_"$separation"mm_st-x_"${mat/\ /mm_st-mat_}"_n-particles_"$n_particles"_"$file_name
+    file=$output_dir"sep_"$separation"mm_st-x_"${mat/\ /mm_st-mat_}"_n-particles_"$n_particles"_"$file_name
     echo $file
     cmd="$exe $separation ${mat/\ / G4_} $n_particles $file $log"
     echo $cmd
